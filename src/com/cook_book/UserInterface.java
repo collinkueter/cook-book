930be5df3f7eb519caf3f5f8e261f9a6506b5280
Collin Kueter <collinkueter@gmail.com>
2016-03-07 18:00:50 -0500

    Intial commit

---
+++ src/com/cook_book/UserInterface.java
@@ -0,0 +1,14 @@
+package com.cook_book;
+
+/**
+ * Created by collinkueter on 3/7/16.
+ */
+public interface UserInterface {
+
+    public void searchByIngredient();
+    public void searchByName();
+    public void addRecipe();
+    public void removeRecipe();
+    public void addGroceryListItem();
+    public void removeGroceryListItem();
+}
